76b5ef19b2bd328d6b5c850d90d6f63bbfecbd77
Josh Click <yq272@nyu.edu>
2013-02-14 02:12:35 -0500

    push

--- cupid.js
+++ cupid.js
@@ -5,22 +5,42 @@ init();
 function init() {
 
 	var canv = document.getElementById("siteCanvas");
+	ctx = canv.getContext('2d');
+
 
 	var file = "story.txt";
-	storyArr = ''; //your array
+	storyArr = []; //your array
 
-	jQuery.get(file, function(data) {
-	   alert(data);
+	$.get(file, function(data) {
+	   storyArr = data.split("\n");
 	});
+	
+	
+	setTimeout(main, 300);
 
-/*
-	for (var i = 0; i<storyArr.length; i++) {
-		console.log(storyArr[i]);
-	}
-*/
 }
 
 
+function main() {
+
+	for (var i = 0; i<storyArr.length; i++) {
+		drawLine((storyArr[i]));
+	}
+	
+	
+}
+
+function drawLine(line) {
+
+	var yCoord = 300
+
+	var xCoord = 50;
+
+	ctx.fillStyle = "#000";
+
+	ctx.fillText(line, xCoord, yCoord);
+
+}
 
 /*
 .panel {
