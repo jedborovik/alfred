286d17acab72d320f43182064d112cfce60dfd05
Josh Click <yq272@nyu.edu>
2013-02-14 01:24:04 -0500

    asdf

---
+++ cupid.js
@@ -0,0 +1,25 @@
+
+init();
+
+function init() {
+
+	var canv = document.getElementById("siteCanvas");
+
+	var file = "story.txt";
+	var resultArr = [], numberOfFiles = 1000;
+
+	function getHandler() {
+		return function(data) {
+			resultArr[] = data;
+		}
+	}
+	$.ajax(file + ".txt").done(getHandler());
+
+}
+
+/*
+.panel {
+color: #808080;
+font-size: 1.385em;
+font-weight: bolder;
+*/
